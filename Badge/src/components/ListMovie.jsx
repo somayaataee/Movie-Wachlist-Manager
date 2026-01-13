6e6e658ec949c939e0c4b87ba2fe1c987b5a8002
Somaya Ataie <somayaatee0@gmail.com>
2026-01-13 20:56:19 +0430

    create component

---
+++ Badge/src/components/ListMovie.jsx
@@ -0,0 +1,28 @@
+export default function ListMovies(){
+
+    const filteredrMovies = ListMovies.filter((movie)=>{
+        if(filter === "All") return true;
+        if(filter === "watched") return movie.watch;
+        if(filter === "unwatched") return !movie.watch
+    })
+    return(
+        <>
+        <h2>Movie List</h2>
+        {filteredrMovies === 0 && (<p>No movies found. add one!</p>)}
+
+        <ul>
+            {filteredrMovies.map((movie)=>
+            <li key={id}><strong>{movie.title}</strong><p>{movie.genre} | {""} {movie.watch ? "watched" 
+                : "unwatched" } </p>
+                
+                  <button onClick={()=> deleteMovie(movie.id)}>Delete</button>
+                  <button onClick={()=> toggeleMovie(movie.id)}>Toggel</button>
+                </li>
+            )}
+        </ul>
+
+        
+        
+        </>
+    )
+}
